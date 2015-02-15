35bf87bfc6e84ff9c65bebdcea15bdb760795b32
James Powell <jamespo@gmail.com>
2015-02-15 20:01:45 +0000

    allow choice of format through CGI argument

--- check_nrpe_http/check_nrpe_http_cgi.py
+++ check_nrpe_http/check_nrpe_http_cgi.py
@@ -27,7 +27,7 @@ def read_conf(conffile):
     else:
         return None
 
-def print_http_headers(output_format = 'json'):
+def print_http_headers(output_format):
     '''print http header'''
     if output_format == 'debug':
         print "Content-Type: text/html"  # DEBUG
@@ -51,26 +51,28 @@ def output_prometheus(results):
     statdict = dict(map((lambda y: y.split('=')), statlines))
     return output_plain(statdict)
 
-def print_results(output_format, results):
+def print_results(results, output_format = 'json'):
+    '''output the results'''
     print_http_headers(output_format)
-    if output_format == 'json':
-        print json.dumps(results)
-    elif output_format == 'text':
+    if output_format == 'text':
         print output_plain(results)
     elif output_format == 'prometheus':
         print output_prometheus(results)
+    else: # json, debug
+        print json.dumps(results)
 
 def run_check(conf):
     '''get & verify cgi arguments then run the check'''
     form = cgi.FieldStorage()
     args  = shlex.split(form.getfirst("args", ""))
     check  = form.getfirst("check", "")
+    output_format = form.getfirst("format", "json")
     check_results = {}
 
     # verify check isn't dir traversal attempt, etc
     if not re.match('\w+\.?\w*$', check):
         return({ 'output' : 'check badly formed', 
-                 'returncode' : 3 })
+                 'returncode' : 3 }, output_format)
 
     plugin_dir = conf.get('main', 'plugin_dir')
     fullcheck = os.path.join(plugin_dir, check)
@@ -90,29 +92,30 @@ def run_check(conf):
         check_results['returncode']= 3
 
     check_results['command'] = check
-    return check_results
+    return (check_results, output_format)
 
     
 def main():
+    '''read conf, check if valid, run & print check if so'''
     conf = read_conf(NRPE_HTTP_CONF)
     allowed_ip = conf.get('main', 'allowed_ip')
-    output_format = conf.get('main', 'format') # one of json,debug,text
+    output_format = 'json' # default
     if conf is None:
-        print_results(output_format, { 'output' : 'No conf file', 
-                    'returncode' : 3 })
+        print_results({ 'output' : 'No conf file', 'returncode' : 3 },
+                      output_format)
     elif conf.getboolean('main', 'secure') and (os.environ.get('HTTPS', 'off') != 'on'):
         # reject if required to run under HTTPS & it isn't
-        print_results(output_format, { 'output' : 'Not called via HTTPS', 
-                        'returncode' : 3 })
+        print_results({ 'output' : 'Not called via HTTPS', 'returncode' : 3 },
+                      output_format)
     elif (allowed_ip is not None) and (os.environ['REMOTE_ADDR'] not in
                                        allowed_ip.split(',')):
         # IP calling check not in permitted list
-        print_results(output_format, { 'output' : 'Not allowed from %s' % os.environ['REMOTE_ADDR'], 
-                        'returncode' : 3 })
+        print_results({ 'output' : 'Not allowed from %s' % os.environ['REMOTE_ADDR'], 
+                        'returncode' : 3 }, output_format)
     else:
         # all good
-        check_results = run_check(conf)
-        print_results(output_format, check_results)
+        (check_results, output_format) = run_check(conf)
+        print_results(check_results, output_format)
 
 if __name__ == '__main__':
     main()
