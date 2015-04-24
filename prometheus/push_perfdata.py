28b690fd3537b1e94a02ba515d1626f7471d6f7c
James Powell <jamespo@gmail.com>
2015-04-24 10:53:23 +0100

    changed list build up to iterator

--- prometheus/push_perfdata.py
+++ prometheus/push_perfdata.py
@@ -18,9 +18,7 @@ def readconf():
     config = ConfigParser.ConfigParser()
     config.read(['/etc/push_perfdata.conf', 
                  os.path.expanduser('~/.config/.push_perfdata.conf')])
-    conf_vals = [ config.get('Main', item) for item in 
-                  ('username','password','host','db')]
-    return tuple(conf_vals)
+    return config
 
 def split_perfdata(perf_str):
     """
@@ -28,28 +26,29 @@ def split_perfdata(perf_str):
     """
     fields = perf_str.split(";")
     split_re = re.compile('([\d\w_-]+)=([\d.\w%]+)')
-    matches = []
     for field in fields:
         match = split_re.search(field)
         if match:
-            matches.append((match.group(1), match.group(2)))
-    return matches
+            yield (match.group(1), match.group(2))
+
 
 def main():
     host_sql = "select alias, perfdata from icinga_hosts ih, icinga_hoststatus ihs where ih.host_object_id = ihs.host_object_id"
     service_sql = "select display_name, perfdata from icinga_services ics, icinga_servicestatus iss where ics.service_object_id = iss.service_object_id and perfdata <> ''"
-    conf_vals = readconf()
-    db = sql.create_engine('mysql://%s:%s@%s/%s' % conf_vals)
+    config = readconf()
+    db_conf_vals = tuple( config.get('Main', item) for item in 
+                          ('username','password','host','db'))
+    db = sql.create_engine('mysql://%s:%s@%s/%s' % db_conf_vals)
 
     str_sql = sql.text(host_sql  )
     results = db.execute(str_sql).fetchall()
     for res in results:
-        print(split_perfdata(res[1]))
+        print(tuple(split_perfdata(res[1])))
 
-        str_sql = sql.text(service_sql)
-        results = db.execute(str_sql).fetchall()
+    str_sql = sql.text(service_sql)
+    results = db.execute(str_sql).fetchall()
     for res in results:
-        print(split_perfdata(res[1]))
+        print(tuple(split_perfdata(res[1])))
 
 if __name__ == '__main__':
     main()
