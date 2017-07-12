d4cfefb9f9ef14d02eea8d5214328906056d307d
James Powell <jamespo@gmail.com>
2017-07-12 11:58:28 +0100

    fix late logic & add minimum train #

--- checks/check_c2c/check_c2c.py
+++ checks/check_c2c/check_c2c.py
@@ -13,6 +13,7 @@
 import urllib2
 from optparse import OptionParser
 from bs4 import BeautifulSoup
+import os
 
 class c2c_util(object):
     @staticmethod
@@ -33,7 +34,8 @@ class c2c_util(object):
         for ul in all_ul:
             if ul.get('class') == [u'boards', u'rowAlternator1', u'clr'] or \
                 ul.get('class') == [u'boards', u'rowAlternator2', u'clr']:
-                #print ul.contents
+                if os.getenv('DEBUG'):
+                    print ul.contents
                 for span in ul.find_all('span'):
                     # get the time
                     if span['class'] == [u'boardsTimeVal']:
@@ -45,7 +47,7 @@ class c2c_util(object):
         return departures
 
     @staticmethod
-    def find_delays(statushtml):
+    def find_delays(statushtml, min_trains):
         if 'There are no direct services' in statushtml:
             return (1, 'No trains found')
         # TODO: other non-results checks
@@ -53,20 +55,22 @@ class c2c_util(object):
             departures = c2c_util.parse_page(statushtml)
             num_trains = len(departures)
             num_ontime = len([train[1] for train in departures if train[1] == 'On Time'])
-            if num_trains == num_trains:
+            if num_trains < min_trains:
+                return (2, 'Only %s trains found (%s required)' % (num_trains, min_trains))
+            elif num_trains == num_ontime:
                 return (0, 'No delays (%s trains found)' % num_trains)
             else:
                 return (2, '%s/%s trains delayed' % (num_trains - num_ontime, num_trains))
 
     @staticmethod
-    def dl_status_page(from_stn, to_stn):
+    def dl_status_page(from_stn, to_stn, min_trains):
         '''download tfl xml, parse and output status'''
         feedurl = 'http://m.journeycheck.com/c2c/getDepartureBoards?fHome=false&from=%s&to=%s&savedRoute=&incidentsType=DEPARTURE_BOARD' % (from_stn, to_stn)
         try:
             livefeed = urllib2.urlopen(feedurl)
             statushtml = livefeed.read()
             #print statushtml # DEBUG
-            (status, statstr) = c2c_util.find_delays(statushtml)
+            (status, statstr) = c2c_util.find_delays(statushtml, min_trains)
         #else:
         #        (status, statstr) = (0, 'All Lines OK')
         except urllib2.URLError:
@@ -80,11 +84,13 @@ class c2c_util(object):
                           dest="from_stn", default = None)
         parser.add_option("-t", "--to", help="Destination Station Code (to)",
                           dest="to_stn", default = None)
+        parser.add_option("-m", "--mintrains", help="Minimum # of trains",
+                          dest="min_trains", default = "3")
         (options, args) = parser.parse_args()
         if options.from_stn is None or options.to_stn is None:
             return (None, None)
         else:
-            return (options.from_stn, options.to_stn)
+            return (options.from_stn, options.to_stn, int(options.min_trains))
 
 
 class c2cfeed(object):
@@ -100,8 +106,8 @@ class c2cfeed(object):
 
 # start of main program
 def main():
-    (from_stn, to_stn) = c2c_util.get_cli_options()
-    (status, statstr) = c2c_util.dl_status_page(from_stn, to_stn)
+    (from_stn, to_stn, min_trains) = c2c_util.get_cli_options()
+    (status, statstr) = c2c_util.dl_status_page(from_stn, to_stn, min_trains)
     print statstr
     exit(status)
 
