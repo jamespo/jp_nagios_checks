3aebb046275a1acbb17902346acadb80f81035e1
James Powell <jamespo@gmail.com>
2019-02-18 12:15:29 +0000

    remove tabs from JSON output

--- qicinga/qicinga/__init__.py
+++ qicinga/qicinga/__init__.py
@@ -49,7 +49,7 @@ def get_page(ic_url, user, pw, hostname):
 
 def read_json(icinga_json):
     '''parse json into data structure'''
-    icinga_status = json.loads(icinga_json)
+    icinga_status = json.loads(icinga_json.replace('\t', ' '))  # cleanup output
     return icinga_status
 
 
