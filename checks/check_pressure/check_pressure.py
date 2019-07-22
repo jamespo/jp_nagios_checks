01680575b4d2006bb815646f401d92de81afc1c0
James Powell <jamespo@gmail.com>
2019-07-22 13:55:58 +0100

    basic cli parsing

--- checks/check_pressure/check_pressure.py
+++ checks/check_pressure/check_pressure.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from optparse import OptionParser
+import sys
 
 def get_cli_args():
     '''get cli args'''
-    return None
+    parser = OptionParser()
+    parser.add_option("-w", dest="warning",  default='cpu10>0.2,fullio10>0.2,fullmemory10>0.2',
+        help="warning thresholds")
+    parser.add_option("-c", dest="critical", default='cpu10>0.5,fullio10>0.5,fullmemory10>0.5',
+        help="critical thresholds")
+    (options, args) = parser.parse_args()
+    return options
+
+def output(rc, msg):
+    '''format rc & output msg & quit'''
+    rc2str = ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
+    print(rc2str[rc] + ': ' + msg)
+    sys.exit(rc)
+
+def cleandata(line):
+    '''parse the pressure output'''
+    return line  # TODO
 
 def read_pressure():
     '''read info under /proc/pressure'''
@@ -12,7 +30,8 @@ def read_pressure():
     for monitor in ('cpu', 'io', 'memory'):
         try:
             with open('/proc/pressure/%s' % monitor) as pres:
-                presstats[monitor] = pres.read()
+                line = pres.read().strip()
+                presstats[monitor] = cleandata(line)
         except:
             return None
     return presstats
@@ -22,8 +41,9 @@ def main():
     '''get args, read pressure'''
     args = get_cli_args()
     presstats = read_pressure()
+    print(presstats)  # DEBUG
     if presstats is None:
-        print("Can't open /proc/pressure/. Supported on 4.2+ kernels only")
+        output(3, "Can't open /proc/pressure/. Supported on 4.2+ kernels only")
 
 
 
