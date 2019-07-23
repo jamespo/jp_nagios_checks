1aa540eaeb74fc14b1d839461a6983347b59fdbe
James Powell <jamespo@gmail.com>
2019-07-23 16:18:17 +0100

    proper parsing

--- checks/check_pressure/check_pressure.py
+++ checks/check_pressure/check_pressure.py
@@ -4,15 +4,19 @@ from __future__ import print_function
 from optparse import OptionParser
 import sys
 
+
 def get_cli_args():
     '''get cli args'''
     parser = OptionParser()
-    parser.add_option("-w", dest="warning",  default='cpu10>0.2,fullio10>0.2,fullmemory10>0.2',
-        help="warning thresholds")
-    parser.add_option("-c", dest="critical", default='cpu10>0.5,fullio10>0.5,fullmemory10>0.5',
-        help="critical thresholds")
+    parser.add_option("-w", dest="warning",
+                      default='cpu10>0.2,fullio10>0.2,fullmemory10>0.2',
+                      help="warning thresholds")
+    parser.add_option("-c", dest="critical",
+                      default='cpu10>0.5,fullio10>0.5,fullmemory10>0.5',
+                      help="critical thresholds")
     (options, args) = parser.parse_args()
     return options
+
 
 def output(rc, msg):
     '''format rc & output msg & quit'''
@@ -20,31 +24,48 @@ def output(rc, msg):
     print(rc2str[rc] + ': ' + msg)
     sys.exit(rc)
 
-def cleandata(line):
-    '''parse the pressure output'''
-    return line  # TODO
 
-def read_pressure():
-    '''read info under /proc/pressure'''
-    presstats = {}
-    for monitor in ('cpu', 'io', 'memory'):
-        try:
-            with open('/proc/pressure/%s' % monitor) as pres:
-                line = pres.read().strip()
-                presstats[monitor] = cleandata(line)
-        except:
-            return None
-    return presstats
+class Pressure():
+    '''store and interrogate /proc/pressure'''
+
+    def __init__(self):
+        self.presstats = Pressure.read_pressure()
+
+    @staticmethod
+    def cleandata(lines):
+        '''parse the pressure output'''
+        parsedlines = {}
+        for line in lines:
+            stattype, stats = line.split(" ", 1)  # get some/full
+            parsedlines[stattype] = {}
+            for stat in stats.split(" "):
+                # get key & value
+                key, val = stat.split("=")
+                parsedlines[stattype][key] = float(val)
+        return parsedlines
+
+    @staticmethod
+    def read_pressure():
+        '''read info under /proc/pressure'''
+        presstats = {}
+        for monitor in ('cpu', 'io', 'memory'):
+            try:
+                with open('/proc/pressure/%s' % monitor) as pres:
+                    # cleanup line(s), split some/full if available & parse via cleandata
+                    presstats[monitor] = Pressure.cleandata(pres.read().strip().split("\n"))
+            except:
+                return None
+        return presstats
+
 
 
 def main():
     '''get args, read pressure'''
     args = get_cli_args()
-    presstats = read_pressure()
-    print(presstats)  # DEBUG
-    if presstats is None:
+    p = Pressure()
+    print(p.presstats)  # DEBUG
+    if p.presstats is None:
         output(3, "Can't open /proc/pressure/. Supported on 4.2+ kernels only")
-
 
 
 if __name__ == '__main__':
